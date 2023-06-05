48decf5900566eb65b870679531fc5b10c5b661e
maxlmuller <maxlmuller@outlook.com>
2023-06-05 16:04:33 -0300

    Atividade 07 - Criação do projeto e componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'controle-financeiro-pessoal';
+  title = 'Atividade 07';
 }
